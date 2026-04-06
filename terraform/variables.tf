e57573a80840ae3f26ef8b3231a82e7c47d32297
Stan <oderaghara5@icloud>
2026-04-06 13:08:07 +0100

    Add CI/CD pipeline for AttendanceCRM and update Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "sql_server_admin_password" {
-  description = "The administrator login password for the SQL Server."
-  type        = string
-  sensitive   = true
-}
-
 variable "hosted_zone_name" {
   type    = string
   default = "attendanceappservice.online"
